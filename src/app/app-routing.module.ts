9f6512208f4841c0573de811c3f443f2d49d45fa
Karla038 <karladk038@gmail.com>
2024-05-31 15:04:34 -0600

    Navbar, Login y Registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductoListaComponent } from './componentes/producto-lista/producto-lista.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', component: ProductoListaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
